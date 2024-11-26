22e3417e16df78c9303c18c13f4d7c2f3fc79f5b
joao <joao.23204708360001@faeterj-rio.edu.br>
2024-11-26 09:03:25 -0300

    last

--- cliente/src/js/getPagamento.js
+++ cliente/src/js/getPagamento.js
@@ -11,14 +11,13 @@ function pag() {
             if (this.status === 200) {
                 const objReturnJSON = JSON.parse(this.responseText);
                 carregarDadosPag(objReturnJSON);
-                console.log(objReturnJSON);
             } else {
                 console.log("Requisição falhou: " + this.status);
             }
         }
     };
 
-    xmlhttp.open("POST", "http://localhost/3DAW-Clinica-de-Estetica/cliente/src/php/pagamento.php   ");
+    xmlhttp.open("POST", "http://localhost/3DAW-Clinica-de-Estetica/cliente/src/php/pagamento.php");
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.send("id=" + idServico);
 }
@@ -26,43 +25,38 @@ function pag() {
 function carregarDadosPag(datas) {
     datas.forEach(data => {
         const resumoCompra = document.getElementById("resumo-compra");
-        const informacoesAdicionais = document.getElementById("informacoes-adicionais");
-
 
         resumoCompra.innerHTML = `
-            <h2>Detalhes do Serviço</h2>
-            <p><strong>Serviço:</strong> ${data.tipo}</p>
-            <p><strong>Descrição:</strong> ${data.descricao}</p>
-            <p><strong>Profissional:</strong> ${data.nomeFuncionario}</p>
-            <p><strong>Preço:</strong> R$ ${data.preco}</p>
-            <img src="" alt>
-
+            <img src="${data.caminho}" alt="${data.altImagem} style="width: 0; height: 0;"">
             <h2>Preço: R$${data.preco}</h2>
-            <p><strong>Serviço: ${data.tipo}</strong> </p>
-            <p><strong>Profissional: ${data.nomeFuncionario}</strong></p>
-            <p><strong>Data: ${data.dataServico}</strong></p>
-            <p><strong>Hora: ${data.horarioInicio} - ${data.horarioFim}</strong></p>
-        `;
-        
-        informacoesAdicionais.innerHTML = `
-            <p><strong>Nome do Serviço:</strong> <?php echo $nome_servico; ?></p>
-            <p><strong>Profissional Selecionado:</strong> <?php echo $nome_profissional; ?></p>
-            <p><strong>Tolerância de Atraso: 30 min antes do horário marcado</strong></p>   
+            <p><strong>Serviço: </strong>${data.tipo}</p>
+            <p><strong>Profissional: </strong>${data.nomeFuncionario}</p>
+            <p><strong>Data: </strong>${data.dataServico}</p>
+            <p><strong>Hora: </strong>${data.horarioInicio} - ${data.horarioFim}</p>
+            <p><strong>Profissional Selecionado: </strong>${data.nomeFuncionario}</p>
+            <p><strong>Tolerância de Atraso: </strong>30 min antes do horário marcado</p>   
         `;
     });
 }
 
-resumo-compra
-<div class="foto-compra">Foto da Compra</div>
-                <div class="detalhes-preco">
-                    <h2>Preço: R$ <?php echo number_format($preco, 2, ',', '.'); ?></h2>
-                    <p><strong>Serviço:</strong> <?php echo $nome_servico; ?></p>
-                    <p><strong>Profissional:</strong> <?php echo $nome_profissional; ?></p>
-                    <p><strong>Data e Hora:</strong> <?php echo date("d/m/Y", strtotime($data_servico)) . " às " . $horario_inicio; ?></p>
-                </div>
+function processarPagamento() {
+    const titularCartao = localStorage.getItem("titular-cartao");
+    const cpf = localStorage.getItem("cpf");
+    const numeroCartao = localStorage.getItem("numero-cartao");
+    const validadCartao = localStorage.getItem("validade-cartao");
+    const codigoCartao = localStorage.getItem("codigo-cartao");
 
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+            } else {
+                console.log("Requisição falhou: " + this.status);
+            }
+        }
+    };
 
-informacoes-adicionais
-<p><strong>Nome do Serviço:</strong> <?php echo $nome_servico; ?></p>
-<p><strong>Profissional Selecionado:</strong> <?php echo $nome_profissional; ?></p>
-<p><strong>Tolerância de Atraso: 30 min antes do horário marcado</strong></p>   
+    xmlhttp.open("POST", "http://localhost/3DAW-Clinica-de-Estetica/cliente/src/php/processarPagamento.php");
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("titularCartao=" + titularCartao + "&cpf=" + cpf + "&numeroCartao=" + numeroCartao + "&validadCartao=" + validadCartao + "&codigoCartao=" + codigoCartao);
+}
